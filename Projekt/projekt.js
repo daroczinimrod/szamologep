4b0048e418797dad4d76056e75bf094548411810
rodi <72067090+daroczinimrod@users.noreply.github.com>
2022-11-04 21:26:15 +0100

    Add files via upload

---
+++ Projekt/projekt.js
@@ -0,0 +1,16 @@
+function plusz() 
+{
+document.szamolj.eredmeny.value=document.szamolj.elso.value*1+document.szamolj.masodik.value*1;
+}
+function minusz()
+{
+    document.szamolj.eredmeny.value=document.szamolj.elso.value-document.szamolj.masodik.value;
+}
+function szorzas()
+{
+    document.szamolj.eredmeny.value=document.szamolj.elso.value*document.szamolj.masodik.value;
+}
+function osztas()
+{
+    document.szamolj.eredmeny.value=document.szamolj.elso.value/document.szamolj.masodik.value;
+}
